8910ae914cbd26be53233e9dd80c6ddc5838b2a8
Garrett Kohlrusch <141463025+gkdataio@users.noreply.github.com>
2023-08-20 11:43:59 -0500

    Update applyzer.py
    add -f for multiple domains, ignored an unfixable error from wappalyzer library

--- applyzer.py
+++ applyzer.py
@@ -1,14 +1,10 @@
-from concurrent.futures import ThreadPoolExecutor as executor # pip install futures
-from Wappalyzer import Wappalyzer, WebPage # pip install python-Wappalyzer
-import urllib3, argparse
+from concurrent.futures import ThreadPoolExecutor as Executor  # pip install futures
+from Wappalyzer import Wappalyzer, WebPage  # pip install python-Wappalyzer
+import urllib3, argparse, warnings
 
-urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning) # disable Python SSL warnings !
+warnings.filterwarnings("ignore", category=UserWarning)
 
-from concurrent.futures import ThreadPoolExecutor as Executor
-from Wappalyzer import Wappalyzer, WebPage
-import urllib3, argparse
-
-urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)  # disable Python SSL warnings!
 
 pastel_green = "\033[38;5;120m"
 pastel_blue = "\033[38;5;81m"
@@ -18,13 +14,13 @@ end = "\033[0m"
 
 wappalyzer = Wappalyzer.latest()
 
-print(pastel_blue+"""
+print(pastel_blue + """
                  _                 
   __ _ _ __ _ __| |_  _ ______ _ _ 
  / _` | '_ \ '_ \ | || |_ / -_) '_|
  \__,_| .__/ .__/_|\_, /__\___|_|  
-      |_|  |_|     |__/   @gkdata         
-	"""+end)
+      |_|  |_|     |__/   @gkdata        
+	""" + end)
 
 def check(out, ig, url):
     if not url.startswith('https'):
@@ -43,22 +39,29 @@ def check(out, ig, url):
             print(f"{pastel_purple}Error:{end} [ {bold}{url}{end} ] > {str(e)}")
 
 parser = argparse.ArgumentParser()
-parser.add_argument("-d", "--domain", help="Domain to analyze", type=str, required=True)
-parser.add_argument("-t", "--thread", help="Threads Number - (Default: 1)", type=int)
+parser.add_argument("-d", "--domain", help="Single domain to analyze", type=str)
+parser.add_argument("-f", "--file", help="File containing list of domains to analyze", type=str)
+parser.add_argument("-t", "--thread", help="Threads Number - (Default: 1)", type=int, default=1)
 parser.add_argument("-i", "--ignore", help="To Ignore The Errors", action='store_true')
 parser.add_argument("-o", "--output", help="Save The Results To a File", type=str)
 
 args = parser.parse_args()
 
-domain = str(args.domain)
-threads = str(args.thread)
+threads = args.thread
 ig = str(args.ignore)
 out = str(args.output)
+domains = []
 
-if threads == 'None':
-    threads = 1
+# Read from file if provided
+if args.file:
+    with open(args.file, 'r') as file:
+        domains = [line.strip() for line in file]
+elif args.domain:
+    domains.append(args.domain)
+else:
+    print("Please provide a domain or a file containing domains.")
+    exit(1)
 
-print(f"{pastel_green}[+]{end} Domain: {domain}")
 print(f"{pastel_green}[+]{end} Output: {out}")
 print(f"{pastel_green}[+]{end} Threads: {threads}")
 print(f"{pastel_green}[+]{end} Ignore: {ig}")
@@ -66,4 +69,6 @@ print(f"{pastel_green}[+]{end} Ignore: {ig}")
 print(f"\n{pastel_purple}[+]{end} Results:\n")
 
 with Executor(max_workers=int(threads)) as exe:
-    exe.submit(check, out, ig, domain.strip('\n'))
+    for domain in domains:
+        print(f"{pastel_green}[+]{end} Domain: {domain}")
+        exe.submit(check, out, ig, domain)
